82b0485278eeb95af4f3aaafa6ed2f016df565d9
a-robi98 <robogosims@gmail.com>
2023-05-31 10:36:09 +0200

    added MainTheme

---
+++ Orchard_Project_Library/Orchard_Project_Library.MaintTheme/Manifest.cs
@@ -0,0 +1,10 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+
+[assembly: Theme(
+    Name = "LibraryMainTheme",
+    Author = "AR",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "The library website's main theme."
+)]
